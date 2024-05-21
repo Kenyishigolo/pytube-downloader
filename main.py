40873081f09a501812a8335bb7d7e516ed334ace
Kenyi <kenyishigolo@gmail.com>
2024-05-20 19:16:34 -0500

    Actualizacion para descargar audios, error con la extensión

--- main.py
+++ main.py
@@ -28,6 +28,24 @@ def descargar_video():
     except Exception as e:
         etiqueta_estado.config(text=f"Error: {e}")
 
+#Aun hay errores con la extensión
+def descargar_mp3():
+    url = entrada_url.get()
+    if not url:
+        etiqueta_estado.config(text="Por favor, ingresa una URL")
+        return
+    if not directorio_destino:
+        etiqueta_estado.config(text="Por favor, selecciona un directorio")
+        return
+    try:
+        yt = YouTube(url)
+        mp3 = yt.streams.filter(only_audio=True).first()
+        mp3.download(directorio_destino)
+        etiqueta_estado.config(text="¡Descarga completa!")
+    except Exception as e:
+        etiqueta_estado.config(text=f"Error: {e}")
+
+
 root = tk.Tk()
 root.title("Descargador de Videos de Kuroky")
 
@@ -43,8 +61,11 @@ boton_seleccionar_directorio.pack()
 etiqueta_directorio = tk.Label(root, text="No se ha seleccionado ningún directorio")
 etiqueta_directorio.pack()
 
-boton_descargar = tk.Button(root, text="Descargar", command=descargar_video)
-boton_descargar.pack()
+boton_descargar_video = tk.Button(root, text="Descargar Video", command=descargar_video)
+boton_descargar_video.pack()
+
+boton_descargar_mp3 = tk.Button(root, text="Descargar mp3", command=descargar_mp3)
+boton_descargar_mp3.pack()
 
 etiqueta_estado = tk.Label(root, text="")
 etiqueta_estado.pack()
